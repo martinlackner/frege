71270456c5144552956c3557e6954ccdadb7b6d7
Martin Lackner <unexpected@sent.at>
2020-04-16 12:27:49 +0200

    show confidence intervals

--- bias.py
+++ bias.py
@@ -1,10 +1,18 @@
+""" Bias experiments from
+Paul Harrenstein, Marie-Louise Lackner, and Martin Lackner.
+*A Mathematical Analysis of an Election System Proposed by
+Gottlob Frege*. To appear in Erkenntnis. 2020.
+ Preprint: https://arxiv.org/abs/1907.03643
+"""
+
 from __future__ import print_function
 import apportionment
 import random
 import frege
+from statsmodels.stats.proportion import proportion_confint
 
 
-def exp1():
+def bias_exp():
     runs = 1000000
     num_parties = 5
     random.seed(17091743)
@@ -13,8 +21,10 @@ def exp1():
         distributions.append([random.randrange(1, 1000)
                               for _ in range(num_parties)])
 
+    print("bias means with 95% confidence intervals")
+
     for method in ["quota", "lrm", "dhondt", "saintelague",
-                   "huntington", "adams", "frege", "modfrege"]:
+                   "huntington", "adams", "modfrege"]:
         small = 0
         for r in range(runs):
             seats = 100
@@ -34,7 +44,13 @@ def exp1():
                  < 1. * rep[smallest] / distributions[r][smallest])):
                 small += 1
 
-        print(method, "." * (25 - len(method)), 100. * small / runs, "%")
+        aver = 100. * small / runs
+        conf1, conf2 = proportion_confint(small, runs,
+                                          alpha=0.05, method='wilson')
+
+        print(method + " " + "." * (25 - len(method)), end=" ")
+        print("{:6.3f}% ({:6.3f}%, {:6.3f}%)".format(
+            aver, 100. * conf1, 100. * conf2))
 
 
-exp1()
+bias_exp()
